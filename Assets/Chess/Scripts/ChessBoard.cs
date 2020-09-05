11aa2570f6f8e74b8ddc5160c31abe004d64a4c9
Cooper Saye <coopersaye@gmail.com>
2020-09-04 20:22:35 -0700

    Set up base of ChessBoard and ChessPiece classes.

---
+++ Assets/Chess/Scripts/ChessBoard.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Chess
+{
+    public class ChessBoard : MonoBehaviour
+    {
+        private Piece[,] chessBoard = new Piece[8, 8];
+
+        private Piece GetChessPiece(int x, int y)
+        {
+            return chessBoard[x, y];
+        }
+
+        private void SetChessPiece(int x, int y, Piece chessPiece)
+        {
+            chessBoard[x, y] = chessPiece;
+        }
+    }
+}
---
+++ Assets/Chess/Scripts/ChessPiece.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Chess
+{
+    enum Piece
+    {
+        None,
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King
+    }
+
+    public abstract class ChessPiece : MonoBehaviour
+    {
+        [Header("Attributes")]
+        [SerializeField] private ChessPieceScriptable scriptable = null;
+    }
+}
