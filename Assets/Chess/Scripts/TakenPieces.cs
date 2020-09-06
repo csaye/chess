8a8584ede81d7f19d51f221a0f2eb3e956f38bd8
Cooper Saye <coopersaye@gmail.com>
2020-09-05 20:28:35 -0700

    Set up taken piece display base.

---
+++ Assets/Chess/Scripts/TakenPieces.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Chess
+{
+    public class TakenPieceDisplay : MonoBehaviour
+    {
+        [Header("References")]
+        [SerializeField] private GameObject[] displayChessPieces = new GameObject[12];
+
+        private Transform whitePiecesTransform;
+        private Transform blackPiecesTransform;
+
+        private int whitePieces = 0;
+        private int blackPieces = 0;
+
+        private void Start()
+        {
+            whitePiecesTransform = transform.GetChild(0).transform;
+            blackPiecesTransform = transform.GetChild(1).transform;
+        }
+
+        public void AddDisplayPiece(ChessPiece piece)
+        {
+            if (piece.team == ChessPieceTeam.White)
+            {
+                AddWhitePiece((int)piece.type);
+            }
+            else
+            {
+                AddBlackPiece((int)piece.type);
+            }
+        }
+
+        private void AddWhitePiece(int type)
+        {
+            GameObject piece = displayChessPieces[type];
+            whitePieces++;
+        }
+
+        private void AddBlackPiece(int type)
+        {
+            GameObject piece = displayChessPieces[6 + type];
+            blackPieces++;
+        }
+    }
+}
